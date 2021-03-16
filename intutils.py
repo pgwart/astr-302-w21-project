048377cdd3e3c21efebb439f368c541efd68f1a4
Peter Gwartney <pgwart@uw.edu>
2021-03-16 08:05:35 +0000

    Second commit, minor tweaks

--- intutils.py
+++ intutils.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from astroquery.sdss import SDSS
 from ipywidgets import interactive_output, IntSlider, FloatSlider, Layout, Text, jslink, GridBox, Dropdown, Button, FloatText
-import pandas as pd
+#import pandas as pd
 
 
 def res(ra, dec, ang):
